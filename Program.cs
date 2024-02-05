985ae74239e5aef51555f3415062e9b38dd8bb01
Logan King <logan.ramzie.king@gmail.com>
2024-02-04 21:18:07 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Author:Logan King
+ * course: Comp003-C#
+ * Purpouse: To show that we can use if and switch commands.
+*/
+namespace COMP003A.Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
